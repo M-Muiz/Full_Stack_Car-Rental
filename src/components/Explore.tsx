7ef6147ce064e8746cec1fecebc0864737996537
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-01 21:35:08 +0500

    complete cardtitle

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -1,12 +1,21 @@
 import styles from '@/utils/styles'
 import React from 'react'
 import TitleText from './TitleText'
+import exploreWorlds from '@/utils/exploreWorlds'
+import ExploreCard from './ExploreCard'
 
 const Explore = () => {
     return (
         <section className='px-6 py-10'>
             <div className={`${styles.innerWidth} mx-auto flex flex-col`}>
-                <TitleText />
+                <TitleText title={
+                    <span className='text-gray-900'>Why Ride With Us?</span>
+                } />
+                <div className="my-9 flex flex-col lg:flex-row min-h-[50vh] gap-8 lg:max-32">
+                    {exploreWorlds?.map((item, index) => (
+                        <ExploreCard key={index} item={item} />
+                    ))}
+                </div>
             </div>
         </section>
     )
---
+++ src/components/ExploreCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExploreCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExploreCard
