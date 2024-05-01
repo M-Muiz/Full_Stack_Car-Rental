a645f379baac35c2733c5e7f05a8611d0ec933bf
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-01 21:57:06 +0500

    complete explore

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -9,11 +9,11 @@ const Explore = () => {
         <section className='px-6 py-10'>
             <div className={`${styles.innerWidth} mx-auto flex flex-col`}>
                 <TitleText title={
-                    <span className='text-gray-900'>Why Ride With Us?</span>
+                    <span className='text-gray-900'>Why <span className='text-main-color'>Ride</span> With <span className='text-main-color'>Us</span> ?</span>
                 } />
                 <div className="my-9 flex flex-col lg:flex-row min-h-[50vh] gap-8 lg:max-32">
                     {exploreWorlds?.map((item, index) => (
-                        <ExploreCard key={index} {...item}/>
+                        <ExploreCard key={index} {...item} />
                     ))}
                 </div>
             </div>
